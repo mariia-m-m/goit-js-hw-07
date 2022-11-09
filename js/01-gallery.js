88439d6bf2536f3221a7d364ed28222476de3c89
Mariia Makarkina <mariiamak25@gmail.com>
2022-11-10 01:13:21 +0200

    1 і 2 задачі

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryConteiner = document.querySelector('div.gallery');
+const items = createGallery(galleryItems);
 
-console.log(galleryItems);
+// Задача 1
+// №1
+
+
+function createGallery(galleryItems) {
+  return galleryItems.map(({preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" 
+  href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}" 
+    />
+  </a>
+</div>`
+    }).join(``)
+
+}
+
+galleryConteiner.insertAdjacentHTML('beforeend', items)
+
+// №2
+
+galleryConteiner.addEventListener('click', onGalleryContainerClick)
+
+function onGalleryContainerClick(e) {
+    const imageSource = e.target.dataset.source;
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    } else {
+        const instance = basicLightbox.create(`
+    <img src="${imageSource}" width="800" height="600">
+`)
+        instance.show();
+
+        }
+}
+
+
+
+
+
+   // galleryConteiner.addEventListener('keydown', onGalleryContainerEscapeClick)
+    // function onGalleryContainerEscapeClick(e) {
+    //     if (e.code === "Escape") {
+    //         instance.close();
+    //     }
+    // }
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryConteiner = document.querySelector('ul.gallery');
+const items = createGallery(galleryItems);
+
+
+
+
+function createGallery(galleryItems) {
+  return galleryItems.map(({preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join(``)
+
+}
+
+galleryConteiner.insertAdjacentHTML('beforeend', items)
+
+galleryConteiner.addEventListener('click', onGalleryContainerClick)
+
+function onGalleryContainerClick(e) {
+    const imageSource = e.target.dataset.source;
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    } else {
+var lightbox = new SimpleLightbox('.gallery a', { 
+captionsData: "alt",
+captionDelay: 250,
+});
+    }
+}
